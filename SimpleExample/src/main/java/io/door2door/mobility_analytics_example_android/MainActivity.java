e9aee9b619817991cc74fd96c643c4b033e699c8
Zlatko Stamatov <Zlatko.Stamatov@gmail.com>
2016-10-31 16:54:24 +0100

    MA-15: Initial import.

---
+++ SimpleExample/src/main/java/io/door2door/mobility_analytics_example_android/MainActivity.java
@@ -0,0 +1,54 @@
+package io.door2door.mobility_analytics_example_android;
+
+import android.app.Application;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.door2door.analytics.api.MobilityAnalytics;
+import io.door2door.analytics.api.model.CreateTripEvent;
+import io.door2door.analytics.api.model.InitializationParameters;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button recordAnEventButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recordAnEventButton = (Button) findViewById(R.id.recordAnEventButton);
+        recordAnEventButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        SimpleExampleApplication application = (SimpleExampleApplication) getApplication();
+        MobilityAnalytics mobilityAnalytics = application.getMobilityAnalytics();
+
+        // prepare a sample event for tracking a trip creation
+        CreateTripEvent event = new CreateTripEvent.CreateTripEventBuilder()
+                .setOriginLatitude(52.529919)
+                .setOriginLongitude(13.403067)
+                .setOriginName("Door2Door HQ")
+                .setOriginStreet("Torstrasse 109")
+                .setOriginCity("Berlin")
+                .setOriginPostalCode("10119")
+                .setOriginCountry("Germany")
+                .setDestinationLatitude(52.522258)
+                .setDestinationLongitude(13.412678)
+                .setDestinationName("Alexanderplatz")
+                .setDestinationStreet("AlexanderplatzStreet")
+                .setDestinationCity("BerlinCity")
+                .setDestinationPostalCode("10178")
+                .setDestinationCountry("GermanyCountry")
+                .build();
+
+        // send the event to the mobility analytics instance
+        mobilityAnalytics.recordEvent(event);
+    }
+}
---
+++ SimpleExample/src/main/java/io/door2door/mobility_analytics_example_android/SimpleExampleApplication.java
@@ -0,0 +1,38 @@
+package io.door2door.mobility_analytics_example_android;
+
+import android.app.Application;
+
+import io.door2door.analytics.api.MobilityAnalytics;
+import io.door2door.analytics.api.model.Environment;
+import io.door2door.analytics.api.model.InitializationParameters;
+
+/**
+ * Extension of the application class.
+ */
+public class SimpleExampleApplication extends Application {
+
+    // mobility analytics application singleton instance
+    private MobilityAnalytics mobilityAnalytics;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    public MobilityAnalytics getMobilityAnalytics() {
+        if (mobilityAnalytics == null) {
+            // setup the initializations parameters for the mobility analytics
+            InitializationParameters initializationParameters = new InitializationParameters();
+            initializationParameters.setApplicationName("SimpleExample");
+            initializationParameters.setVersionName("1.0.0");
+            initializationParameters.setLoggerEnabled(true);
+            initializationParameters.setEnvironment(Environment.TEST);
+
+            // setup the mobility analytics application singleton instance
+            mobilityAnalytics = new MobilityAnalytics(this, initializationParameters);
+        }
+
+        return mobilityAnalytics;
+    }
+
+}
